6a36d21b8b1ed77592c3225ee768f204d3375bd7
Alex fdez <Alex fdez>
2017-07-13 23:38:20 +0200

    Agregar archivos de proyecto.

---
+++ webapicore20/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapicore20.wwwroot
+{
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+    }
+}
